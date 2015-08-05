6869779cd53861a1007feb89c233a71fd27cd3d6
Fabien LOISON <flo@flogisoft.com>
2015-08-05 13:56:58 +0200

    Tests

--- test/stonejs_test.js
+++ test/stonejs_test.js
@@ -27,22 +27,13 @@ exports.stonejs = {
     // setup here if necessary
     done();
   },
-  default_options: function(test) {
+  default: function(test) {
     test.expect(1);
 
-    var actual = grunt.file.read('tmp/default_options');
-    var expected = grunt.file.read('test/expected/default_options');
-    test.equal(actual, expected, 'should describe what the default behavior is.');
+    var actual = grunt.file.read('tmp/catalog.json');
+    var expected = grunt.file.read('test/expected/catalog.json');
+    test.equal(actual, expected, 'should built the complied catalog.');
 
     test.done();
-  },
-  custom_options: function(test) {
-    test.expect(1);
-
-    var actual = grunt.file.read('tmp/custom_options');
-    var expected = grunt.file.read('test/expected/custom_options');
-    test.equal(actual, expected, 'should describe what the custom option(s) behavior is.');
-
-    test.done();
-  },
+  }
 };
